ff979eed7d922516044d6ba83c0999571a8fa53e
Harivishnu <root@11CPU0126L.amritavidya.edu>
2016-06-03 12:59:24 +0530

    secund commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from registration.views import  *
 from registration import urls as reg_urls
+from workshop.views import anonymous_required
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'register/', include(reg_urls)),
+url(r'^user/login/$',anonymous_required(auth_views.login),
+    {'template_name': 'register/in.html'},
+    name='login'),
+
 ]
