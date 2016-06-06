4c9edf87059225445a940ae84d56aff50ba5a86e
Harivishnu <root@11CPU0126L.amritavidya.edu>
2016-06-06 15:43:12 +0530

    Fourth commit

--- registration/admin.py
+++ registration/admin.py
@@ -9,3 +9,8 @@ from registration.models import *
 class UserAdmin(admin.ModelAdmin):
 	pass
 admin.site.register(User, UserAdmin)
+
+
+class ChocolateAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Chocolate, ChocolateAdmin)
