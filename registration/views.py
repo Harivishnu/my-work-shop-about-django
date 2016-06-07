defaaca73d2b0559f1e6418616240a683bd448af
Harivishnu <root@11CPU0126L.amritavidya.edu>
2016-06-07 15:29:11 +0530

    Fourth commit

--- registration/views.py
+++ registration/views.py
@@ -6,6 +6,18 @@ from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
 from django.views.generic.edit import FormView, UpdateView
 from registration.forms import *
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+
+
+class CurrentUserMixin(object):
+	model = User
+
+
+	def get_object(self, *args, **kwargs):
+		try: obj = super(CurrentUserMixin, self).get_object(*args, **kwargs)
+		except AttributeError : obj = self.request.user
+		return obj
+
 
 class HomePageView( ListView):
 	template_name = "index.html"
@@ -20,8 +32,8 @@ class UserRegistrationView(AnonymousRequiredMixin, FormView):
 	success_url = '/register/user/success/'
 
 	def form_valid(self, form):
-	    form.save()
-	    return FormView.form_valid(self, form)
+		form.save()
+		return FormView.form_valid(self, form)
 
 
 class AddChocolate(FormView):
@@ -32,3 +44,22 @@ class AddChocolate(FormView):
 	def form_valid(self, form):
 		form.save()
 		return FormView.form_valid(self, form)
+
+
+class ChocolateDetailsView(DetailView):
+	template_name = "chocolate_detail.html"
+
+	def get_object(self, queryset=None):
+		choco_id = self.kwargs['choco_id']
+		obj = Chocolate.objects.get(id=choco_id)
+		if obj:
+			return obj
+		else:
+			raise Http404("No details Found.")
+
+
+class UserProfileUpdateView(LoginRequiredMixin, CurrentUserMixin, UpdateView):
+	model = User
+	fields = user_fields + user_extra_fields
+	template_name_suffix = '_update_form'
+	success_url = '/register/user/profile/'
